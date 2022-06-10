26804c67774e4f78c356312226f7c6c25fd3ec66
Hexcede <8394472+Hexcede@users.noreply.github.com>
2022-06-10 05:49:36 -0400

    Add Util.isCFunction

--- src/Util.lua
+++ src/Util.lua
@@ -2,4 +2,8 @@ local Util = {}
 function Util.unpack(tab)
 	return table.unpack(tab, 1, rawget(tab, "n"))
 end
+function Util.isCFunction(func)
+	local source, line = debug.info(func, "sl")
+	return source == "[C]" and line == -1
+end
 return Util
