09cfca74f665d274cb68a0951860e9c911ca0ffa
Hexcede <8394472+Hexcede@users.noreply.github.com>
2022-06-10 05:49:50 -0400

    Add Util.copy

--- src/Util.lua
+++ src/Util.lua
@@ -2,6 +2,13 @@ local Util = {}
 function Util.unpack(tab)
 	return table.unpack(tab, 1, rawget(tab, "n"))
 end
+function Util.copy(tab)
+	local copy = table.create(#tab)
+	for index, value in pairs(tab) do
+		copy[index] = value
+	end
+	return copy
+end
 function Util.isCFunction(func)
 	local source, line = debug.info(func, "sl")
 	return source == "[C]" and line == -1
