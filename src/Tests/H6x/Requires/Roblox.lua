e6982721f56786dd8a2175cb044fe86c4e45294c
Hexcede <8394472+Hexcede@users.noreply.github.com>
2022-06-10 05:51:23 -0400

    Update Roblox require test

--- src/Tests/H6x/Requires/Roblox.lua
+++ src/Tests/H6x/Requires/Roblox.lua
@@ -2,10 +2,9 @@ return function(H6x)
 	local sandbox = H6x.Sandbox.new()
 
 	sandbox:AllowInstances()
-	sandbox:ExceptReference(script)
 	sandbox:SetRequireMode("roblox")
 
 	assert((sandbox:ExecuteString([[
 		return pcall(require, ...)
-	]], script)), "Require failed when requires are Roblox-like.")
+	]], script:Clone())), "Require failed when requires are Roblox-like.")
 end
